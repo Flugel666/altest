af5bcafcc5ccccbbf8d1db5d86d9b3c8a73da621
Zuosizhu <2839299264@qq.com>
2024-07-11 16:30:36 +0800

    Add: PR7 presets and filters

--- module/research/preset_generator.py
+++ module/research/preset_generator.py
@@ -80,6 +80,8 @@ def translate(string: str, target='series_4_tenrai_only_cube', for_simulate=Fals
 
 def convert_name(name, series):
     name = re.sub(r'series_\d', f'series_{series}', name)
+    if 'series_7' in name:
+        name = name.replace('tenrai', 'la9')
     if 'series_6' in name:
         name = name.replace('tenrai', '203')
     if 'series_5' in name:
@@ -100,7 +102,7 @@ if __name__ == '__main__':
     Value(
         FILTER_STRING_CHEAPEST='Q1 > Q2 > T3 > T4 > Q4 > C6 > T6 > C8 > C12 > G1.5 > D2.5 > G2.5 > D5 > Q0.5 > G4 > D8 > H1 > H2 > H0.5 > D0.5 > H4')
     with Dict('DICT_FILTER_PRESET'):
-        for series in [6, 5, 4, 3, 2]:
+        for series in [7, 6, 5, 4, 3, 2]:
             def new_filter(**kwargs):
                 for k, v in kwargs.items():
                     k = convert_name(k, series)
@@ -109,7 +111,7 @@ if __name__ == '__main__':
 
             # 1
             Comment("""
-                Goal: DR_blurprint=0, PRY_blueprint=0, tanrai_blueprint=150
+                Goal: DR_blueprint=0, PRY_blueprint=0, tanrai_blueprint=150
                 Average time cost: 153.41706666666678
                 Average rewards: [238.69016631 238.37881965 529.71190834 528.92520834 528.39586667 150.07973333]
             """)
@@ -122,7 +124,7 @@ if __name__ == '__main__':
             """)
             # 2
             Comment("""
-                Goal: DR_blurprint=0, PRY_blueprint=0, tanrai_blueprint=150
+                Goal: DR_blueprint=0, PRY_blueprint=0, tanrai_blueprint=150
                 Average time cost: 161.37177965277806
                 Average rewards: [241.92774575 241.13046242 421.82134358 421.04494941 420.46893024 150.07799978]
             """)
@@ -135,7 +137,7 @@ if __name__ == '__main__':
             """)
             # 5
             Comment("""
-                Goal: DR_blurprint=513, PRY_blueprint=343, tanrai_blueprint=100
+                Goal: DR_blueprint=513, PRY_blueprint=343, tanrai_blueprint=100
                 Average time cost: 124.67622465277958
                 Average rewards: [531.93022864 529.81919864 510.27473326 510.18530159 510.11215826 100.8088164]
             """)
@@ -148,7 +150,7 @@ if __name__ == '__main__':
             """)
             # 6
             Comment("""
-                Goal: DR_blurprint=513, PRY_blueprint=343, tanrai_blueprint=100
+                Goal: DR_blueprint=513, PRY_blueprint=343, tanrai_blueprint=100
                 Average time cost: 143.56399131945145
                 Average rewards: [520.06195858 519.19883191 392.86544828 392.64870495 392.49383995 102.2368499]
             """)
@@ -161,7 +163,7 @@ if __name__ == '__main__':
             """)
             # 3
             Comment("""
-                Goal: DR_blurprint=513, PRY_blueprint=343, tanrai_blueprint=0
+                Goal: DR_blueprint=513, PRY_blueprint=343, tanrai_blueprint=0
                 Average time cost: 82.0121088194467
                 Average rewards: [519.0311752  514.64003687 653.77171198 653.72126532 653.66129615 26.97694791]
             """)
@@ -174,7 +176,7 @@ if __name__ == '__main__':
             """)
             # 4
             Comment("""
-                Goal: DR_blurprint=513, PRY_blueprint=343, tanrai_blueprint=0
+                Goal: DR_blueprint=513, PRY_blueprint=343, tanrai_blueprint=0
                 Average time cost: 124.71616166666873
                 Average rewards: [514.96354877 514.70099977 355.58865468 354.96831385 354.66888635 56.48432238]
             """)
