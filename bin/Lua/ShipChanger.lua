6e646ca879bf636ea5921e6f0f7444a4ec5685b4
Zuosizhu <2839299264@qq.com>
2024-07-19 18:22:06 +0800

    Fix: A very accurate but shit way to change ShipType

--- bin/Lua/ShipChanger.lua
+++ bin/Lua/ShipChanger.lua
@@ -4,9 +4,75 @@ function string.split(s, p)
     return Result
 end
 
+function num_to_hex(n)
+	local hex="0x"
+	local num=n
+	local tmp=0
+	local ttable = {
+		[1] = "1",
+		[2] = "2",
+		[3] = "3",
+		[4] = "4",
+		[5] = "5",
+		[6] = "6",
+		[7] = "7",
+		[8] = "8",
+		[9] = "9",
+		[10] = "A",
+		[11] = "B",
+		[12] = "C",
+		[13] = "D",
+		[14] = "E",
+		[15] = "F",
+		[16] = "0"
+	}
+	while 1==1 do
+		tmp = num % 16
+		if tmp ==0 then tmp =16 end
+	    hex = ttable[tmp] .. hex
+		num = num // 16
+		if num == 0 then break end
+		end
+	return string.sub(hex,1,-3)
+end
+
+function LimitedChange(SearchResults, ResultsCount, ShipId_List,ShipTypeId_List, TargetShipTypeId_List, n, ggType)
+	local PossibleList, tempList = {}
+	for j = 1, ResultsCount do
+		if string.sub(tostring(SearchResults[j]['value']),1,-3) == ShipTypeId_List[n] then
+			if tempList == nil then
+				tempList = {[1]=SearchResults[j]}
+			else table.insert(tempList, SearchResults[j]) end
+	end
+		if j == ResultsCount then
+			if tempList == nil then goto continue end
+			if PossibleList == nil then PossibleList = {[1]=tempList[#tempList]}
+			else table.insert(PossibleList, tempList[#tempList]) end
+			tempList = {}
+			goto continue
+		end
+		if string.sub(tostring(SearchResults[j+1]['value']),1,-3) == ShipId_List[n] then
+			if tempList == nil then goto continue end
+			if PossibleList == nil then PossibleList = {[1]=tempList[#tempList]}
+			else table.insert(PossibleList, tempList[#tempList]) end
+			tempList = {}
+		end
+		::continue::
+	end
+	if PossibleList == nil then k=1
+	else
+		for j = 1, #PossibleList do
+			gg.clearResults()
+			gg.searchAddress(num_to_hex(PossibleList[j]['address']),-1,ggType)
+			gg.getResults(1024,0,nil,nil,nil,nil,ggType)
+			gg.editAll(TargetShipTypeId_List[n], ggType)
+		end
+	end
+end
+
 function ChangeShip()
     local Result = gg.prompt({"舰船Id", "舰船稀有度", "舰船星级", "当前舰种Id", "目标舰种Id"}
-			 		    	,{} 
+			 		    	,{101041,2,1,1,3}
 			 		    	,{"number", "number", "number", "number", "number"})
 
 	local ShipIdList = string.split(tostring(Result[1]), ";")
@@ -21,16 +87,16 @@ function ChangeShip()
 
 	for i = 1, #ShipIdList do
 		gg.searchNumber(ShipIdList[i]..";"..ShipRarityList[i]..";"..ShipStarList[i]..";"..ShipTypeIdList[i].."::610", gg.TYPE_DOUBLE)
-		gg.refineNumber(ShipTypeIdList[i], gg.TYPE_DOUBLE)
+		local ResultCount = gg.getResultsCount()
 		local SearchResult =  gg.getResults(1024)
 		if next(SearchResult) == nil then
 			gg.clearResults()
 			gg.searchNumber(ShipIdList[i]..";"..ShipRarityList[i]..";"..ShipStarList[i]..";"..ShipTypeIdList[i].."::610", gg.TYPE_DWORD)
-		    gg.refineNumber(ShipTypeIdList[i], gg.TYPE_DWORD)
-		    gg.getResults(1024)
-			gg.editAll(TargetShipTypeIdList[i], gg.TYPE_DWORD)
+			ResultCount = gg.getResultsCount()
+			SearchResult =  gg.getResults(1024)
+			LimitedChange(SearchResult,ResultCount,ShipIdList,ShipTypeIdList,TargetShipTypeIdList,i,gg.TYPE_DWORD)
 		else
-			gg.editAll(TargetShipTypeIdList[i], gg.TYPE_DOUBLE)
+			LimitedChange(SearchResult,ResultCount,ShipIdList,ShipTypeIdList,TargetShipTypeIdList,i,gg.TYPE_DOUBLE)
 		end
 		gg.clearResults()
 	end
